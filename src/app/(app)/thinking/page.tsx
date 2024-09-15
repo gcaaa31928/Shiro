aecd57aa4e21d59536046e33a5bb7abd775d8224
Red Huang <gcaaa31928@gmail.com>
2024-09-15 20:18:30 +0800

    [wip]: keep going

--- src/app/(app)/thinking/page.tsx
+++ src/app/(app)/thinking/page.tsx
@@ -45,7 +45,6 @@ export default function Page() {
     <div>
       <header className="prose">
         <h1>思考</h1>
-        <h3>谢谢你听我诉说</h3>
       </header>
 
       <main className="mt-10">
--- src/components/modules/comment/CommentBox/ActionBar.tsx
+++ src/components/modules/comment/CommentBox/ActionBar.tsx
@@ -54,7 +54,7 @@ const WhisperCheckbox = () => {
           setter('isWhisper', checked)
         }}
       />
-      <span className="label-text text-sm">悄悄话</span>
+      <span className="label-text text-sm">悄悄話</span>
     </label>
   )
 }
@@ -100,8 +100,8 @@ export const CommentBoxActionBar: Component = ({ className }) => {
         )}
       >
         <span className="hidden md:inline">
-          支持 <b>Markdown</b> 与{' '}
-          <MLink href="https://docs.github.com/zh/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax">
+          支持 <b>Markdown</b> 與{' '}
+          <MLink href="https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax">
             GFM
           </MLink>
         </span>
@@ -141,7 +141,7 @@ const SubmitButton = () => {
     >
       <TiltedSendIcon className="size-5 text-zinc-800 dark:text-zinc-200" />
       <m.span className="text-sm" layout="size">
-        {isPending ? '送信...' : '送信'}
+        {isPending ? '送出...' : '送出'}
       </m.span>
     </m.button>
   )
